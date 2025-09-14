9c8c5235cbe84d03b65670148e9e8d7c8d8b5360
Skcode292 <skfightersk@gmail.com>
2025-09-14 14:17:11 +0500

    Add files via upload

---
+++ adding multiline comments.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+cout<<" adding multi line comments"<< endl;
+/*coding a program for making
+multiple  line compliments*/
+cout <<"the comments are added successfully ";
+return 0 ;
+}
