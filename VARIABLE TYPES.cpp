e8e7321abdfceddb2cd962ef2178b5d16f493ab5
Skcode292 <skfightersk@gmail.com>
2025-09-14 15:45:56 +0500

    Add files via upload

---
+++ VARIABLE TYPES.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+int mynum = 16975;
+string mystring ="Saud";
+double mydouble = 74.60 ;
+bool mybool = false ;
+char sk ='S';
+cout <<"ALL  VARIABLE TYPES \n";
+cout << mynum <<endl;
+cout << mystring << "\n";
+cout << mydouble << "\n";
+cout << mybool <<endl ;
+cout << sk ;
+return 0 ;
+}
+
