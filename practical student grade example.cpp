8f85900734c8121dc42200ab01f6ef2597b42b48
Skcode292 <skfightersk@gmail.com>
2025-09-14 19:27:32 +0500

    Add files via upload

---
+++ practical student grade example.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+string studentname = "SAUD";
+int studentid = 56;
+int studentage = 23;
+float studentpercentage = 74.60 ;
+char studentgrade = 'B';
+cout<< "STUDENT RESULT\n  ";
+cout << "STUDENT NAME :"<<studentname <<endl ;
+cout <<"STUDENT ID :"<<  studentid<<endl ;
+cout <<"STUDENT AGE : " << studentage <<endl ;
+cout << "STUDENT PERCENTAGE :"<< studentpercentage << endl;
+cout << "STUDENT GRADE :" << studentgrade ;
+return 0;
+}
