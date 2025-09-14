e8149c413890517e9c134a6b93fc553dfdd295d1
Skcode292 <skfightersk@gmail.com>
2025-09-14 23:15:56 +0500

    Add files via upload

---
+++ Calculating Area of rectangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int length;
+int width ;
+cout<< "calculating the area of rectangle\n";
+cout << "Enter length for rectangle\n"; 
+cin >> length ;
+cout <<"Enter width for rectangle\n";
+cin >> width ; 
+int area = length * width ;
+cout << "The area of triangle is\n"<< area;
+return 0 ;
+}
