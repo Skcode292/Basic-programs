b14e84b44584cd8d2519a238e29389de2ceebb9b
Skcode292 <skfightersk@gmail.com>
2025-09-16 11:40:25 +0500

    Add files via upload

---
+++ boolean use.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"BOOLEN TRUE OR FALSE\n";
+bool justdoit = true ;
+bool watchme = true ;
+bool brickbybrick = true;
+bool lagging = false ;
+
+cout <<justdoit <<endl;
+cout <<watchme << endl;
+cout <<brickbybrick << endl ;
+cout << lagging ;
+return 0 ;
+}
