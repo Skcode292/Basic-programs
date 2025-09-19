f57ce6ab074751bc85f862de4f1c7583ebde6637
Skcode292 <skfightersk@gmail.com>
2025-09-19 22:47:27 +0500

    Add files via upload

---
+++ simple public specifier.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+class myclass {
+public:
+int saud=230;
+private : 
+int Khan = 110;
+  };
+  int main() {
+  myclass obj;
+  cout <<obj.saud ;
+  return 0 ;}
