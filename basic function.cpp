67e10eaba26ff25ef8d8a14ea1adba6fe8786fa3
Skcode292 <skfightersk@gmail.com>
2025-09-20 14:49:41 +0500

    Add files via upload

---
+++ basic function.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int sk(int s, int k) {
+int skx=s+k;
+return skx ;}
+int main (){
+int w, p;
+cout << "Enter first number\n";
+cin>>w;
+cout <<"Enter second number\n";
+cin>>p;
+cout<< "The addition is " << sk( w,  p);
+return 0 ;}
