e4b6dda133a34b4e7503f31c5e4242b759392c85
Skcode292 <skfightersk@gmail.com>
2025-11-06 20:10:13 +0500

    Add files via upload

---
+++ simple program for cs201.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+cout << " all questions from 11 to 15 \n";
+int a=100;
+int b=5;
+int c;
+int r;
+int sum;
+float divide;
+int subtract;
+int multiple;
+cout<<"enter r for circle\n";
+cin>> r;
+divide=a/b;
+cout <<divide<<endl;
+subtract=a-b;
+cout <<subtract<<endl;
+multiple=a*b;
+cout <<multiple<<endl;
+sum=a+b;
+cout <<sum<<endl;
+int area = 3.14*r*r;
+cout<<area  <<endl;
+
+int avg=(a+b+sum) /3;
+cout <<avg <<endl;
+
+
+}
