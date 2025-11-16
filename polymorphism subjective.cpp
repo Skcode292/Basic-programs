6a7fa8300442fb1a9f49b3da84a117e84a9aad18
Skcode292 <skfightersk@gmail.com>
2025-11-16 14:36:59 +0500

    Add files via upload

---
+++ polymorphism subjective.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+cout<<" POLYMORPHISM"<<endl;
+cout<<" polymorphism is of two types \n";
+cout<<" 1 Compile time polymorphism"<<endl;
+cout<<" compile time polymorphism is achieved by"<<endl;
+cout<<" 1.1 Function overloading "<<endl;
+cout<<" 1.2 OOperator overloading "<<endl;
+cout<<" \n\n"<<endl;
+cout<<" 2 Run-time polimorphism "<<endl;
+cout<<" Run- time polymorphism is achieved by "<<endl;
+cout<<" 1.1 Virtual Function "<<endl;
+cout<<" ---------Done--------"<<endl;
+
+return 0;
+}
